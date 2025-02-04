26cedc2513dd24296fb43ed4ef46b72921ecd405
SheikhAbdullahAhmed <abdullahaheikh456677@gmail.com>
2025-02-04 20:28:03 +0500

    ✨

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Contact from './Components/Contact/index.jsx'
 import About from './Components/About/index.jsx'
+import Footer from './Components/Footer/index.jsx'
+
 const App = () => {
   return <div>
-    {/* <Contact/> */}
+    <Contact/>
     <About/>
+    <Footer/>
 
   </div>;
 };
